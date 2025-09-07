7bb26708597d94c5d4978480af0030c7626b1187
KshitijaP-25 <kshitija.palwekar24@gmail.com>
2025-09-07 17:58:18 +0530

    Update MultiWindowHandles.java

--- MultiWindowHandles.java
+++ MultiWindowHandles.java
@@ -2,14 +2,12 @@ package selenium.WebAssignment;
 import java.time.Duration;
 import java.util.List;
 import java.util.Set;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class MultiWindowHandles {
@@ -67,3 +65,4 @@ public class MultiWindowHandles {
 	}
 
 }
+
