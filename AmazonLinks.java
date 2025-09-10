e19842f13a1f9f73522d57fefe345bcce59d46ff
KshitijaP-25 <kshitija.palwekar24@gmail.com>
2025-09-10 11:34:39 +0530

    Update AmazonLinks.java

--- AmazonLinks.java
+++ AmazonLinks.java
@@ -15,17 +15,21 @@ public class AmazonLinks {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.manage().window().maximize();
 		driver.get("https://www.amazon.in/");
-		
+		try {
+			driver.findElement(By.tagName("button")).click();
+		}
+		catch(Exception e) {
+			System.out.println("Amazon Homepage is Opened..");
+		}
 	    List<WebElement> links =driver.findElements(By.tagName("a"));
 		System.out.println("All image URLs on Amazon Homepage are:"+links.size());
 		for(WebElement link : links) {
 		String Link = link.getDomAttribute("href");
 		System.out.println(Link +" : "+link.getText());
-		
 		} 
-		
 		Thread.sleep(2000);
-		driver.quit();
+		driver.close();
 	}
 
 }
+
