079b5d216e2ee8973ad13542683f0d79e1435952
KshitijaP-25 <kshitija.palwekar24@gmail.com>
2025-09-25 16:55:14 +0530

    Add files via upload

---
+++ ChromeOptionsDemo.java
@@ -0,0 +1,31 @@
+package selenium.WebAssignment;
+import java.time.Duration;
+import java.util.Arrays;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ChromeOptionsDemo {
+
+	public static void main(String[] args) throws InterruptedException {
+		//Disable Infobar
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions option = new ChromeOptions();
+		option.setExperimentalOption("excludeSwitches",Arrays.asList("enable-automation","disable-infobar"));
+		
+		WebDriver driver = new ChromeDriver(option);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+		driver.manage().window().maximize();
+		driver.get("https://gmail.com");
+		
+		WebElement E=driver.findElement(By.id("identifierId"));
+		E.sendKeys("ABC Testing");
+		System.out.println(E.getAttribute("value"));
+		
+		Thread.sleep(2000);
+		driver.close();
+	}
+}
